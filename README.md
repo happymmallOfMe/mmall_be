aa349aa5d0bf1ce5e062ff15f1e1a2eb54bc50a2
舒云风 <shuyunfengcsdn@gmail.com>
2018-07-17 17:36:45 +0800

    modify for deploy run well

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # mmall_be
 happymmall后端项目
+
+本地测试修改建议：
+1. 将datasource.properties文件中db.username值修改为root。
+2. 将mmall.properties文件中ftp.server.ip值修改为127.0.0.1；
+   ftp.server.http.prefix值修改为http://image.imooc.com/，需将image.imooc.com代理到localhost(在hosts中添加127.0.0.1 image.imooc.com，或者使用Charles)；
+   将alipay.callback.url值修改为 {由natapp生成外网穿透地址}/order/alipay_callback.do，并将该地址添加到支付宝沙箱环境的回调地址栏中；
+   本地ftpserver需设置文件上传路径，且需要与代码中文件上传地址ftp.server.image.path指向一致。
+3. logback.xml文件日志输出地址修改为本地地址。
+4. WEB-INF文件夹下lib中jar包需要额外引入。
--- deploy.sh
+++ deploy.sh
@@ -2,27 +2,27 @@
 
 # 执行本脚本前必有的操作：
 #       必须提前在/git-repository目录下clone项目文件，否则无法连接git远程仓库与本地目录，
-#       后续也无法进行其他git操作；clone完成后，便会在/git-repository目录下新增mmall_learning文件夹；
-#       如有需要，还需赋予用户该文件夹（mmall_learning）的相关权限。
+#       后续也无法进行其他git操作；clone完成后，便会在/git-repository目录下新增mmall_be文件夹；
+#       如有需要，还需赋予用户该文件夹（mmall_be）的相关权限。
 # 问题主要来源：1命令行中sudo的使用；
 #              2用户与文件夹的权限设置；
 #              3github中下载的代码不是新修改代码
 
 
-# 注意：此处目录地址“mmall_learning”需与项目名称一致
-echo "===========1.进入git项目mmall_learning目录=============="
-cd /developer/git-repository/mmall_learning
+# 注意：此处目录地址“mmall_be”需与项目名称一致
+echo "===========1.进入git项目mmall_be目录=============="
+cd /developer/git-repository/mmall_be
 
 
 # 注意：checkout后面的参数需与项目分支名称一致
-echo "================2.git切换分之到v1.0================="
-sudo git checkout v1.0
+echo "================2.git切换分支到mmall_be_v1.0================="
+sudo git checkout mmall_be_v1.0
 
-# 注意：无论是master分支还是还是v1.0分支，他们的ssh地址都是一样的；
+# 注意：无论是master分支还是还是mmall_be_v1.0分支，他们的ssh地址都是一样的；
 #      使用git clone {ssh地址}下载的源代码默认是master分支的代码，
-#      在使用上面“git checkout v1.0”命令切换分支后，在使用下面的
+#      在使用上面“git checkout mmall_be_v1.0”命令切换分支后，在使用下面的
 #      “sudo git fetch”和“sudo git pull”命令更新代码可能不奏效，
-#      所以应该注意检查代码，看是否属于v1.0分支代码。
+#      所以应该注意检查代码，看是否属于mmall_be_v1.0分支代码。
 #      建议：切换分支后，直接执行“git pull”。
 echo "==================3.git fetch======================"
 sudo git fetch
@@ -31,7 +31,7 @@ echo "===================4.git pull======================"
 sudo git pull
 
 # 注意：参数--Dmaven.test.skip=true需置于单引号中，
-#      同时需要注意当前用户对父目录（即/mmall_learning）的权限设置，
+#      同时需要注意当前用户对父目录（即/mmall_be）的权限设置，
 #      此外还需在pom.xml文件的maven-compiler-plugin节点下添加version信息
 echo "=================5.编译并跳过单元测试================"
 # mvn clean package -Dmaven.test.skip=true
@@ -43,7 +43,7 @@ rm /developer/apache-tomcat-9.0.0.M26/webapps/ROOT.war
 
 
 echo "=======7.拷贝编译出来的war包到tomcat下-ROOT.war======="
-cp /developer/git-repository/mmall_learning/target/mmall.war  /developer/apache-tomcat-9.0.0.M26/webapps/ROOT.war
+cp /developer/git-repository/mmall_be/target/mmall.war  /developer/apache-tomcat-9.0.0.M26/webapps/ROOT.war
 
 
 echo "============8.删除tomcat下旧的ROOT文件夹============="
